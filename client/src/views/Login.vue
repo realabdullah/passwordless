78660b207d0c86b09288f96b26e395d3b5aa61a8
realabdullah <abdulodesanmi@gmail.com>
2022-03-28 19:03:25 +0100

    login page

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -8,14 +8,23 @@
       <form
         @submit.prevent=""
         class="bg-gray-700 max-w-md mx-auto rounded p-4">
+
         <label class="block mb-2">
           <span class="block text-gray-400 mb-2">
             Enter your email address
           </span>
           <input 
             type="email"
-            placeholder="blah@meh.com">
+            placeholder="blah@meh.com"
+            class="block w-full rounded bg-gray-800 py-2 px-4 disabled:opacity-50">
         </label>
+
+        <div class="flex justify-end items-center">
+          <input 
+            type="text"
+            value="Login"
+            class="inline-block bg-emerald-400 text-lg text-gray-800 font-bold uppercase rounded px-4 py-2 cursor-pointer disabled:opacity-50">
+        </div>
       </form>
     </section>
   </main>
