e83af1ebd6ab30bc4dd2c89c27247aca69106f69
realabdullah <abdulodesanmi@gmail.com>
2022-03-28 18:37:25 +0100

    router

---
+++ client/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import vm from '../main'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
+  },
+  {
+    path: '/login',
+    name: 'home',
+    component: () => import('../views/Login.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
