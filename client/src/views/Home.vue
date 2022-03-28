e7c37d5779d6b81029b718bd556c6655b7c9450d
realabdullah <abdulodesanmi@gmail.com>
2022-03-28 19:05:43 +0100

    Home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,6 +2,9 @@
   <main class="home">
     <section class="container mx-auto p-4">
       <h1 class="text-4xl mb-8 text-center">Home</h1>
+      <p class="text-gray-500 text-center">
+        Welcome to the home page! <span class="link">Logout?</span>
+      </p>
     </section>
   </main>
 </template>
